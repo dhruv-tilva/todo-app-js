9daddb8c3fbfc820a0b72da9b563f3f63c619193
Dhruv Tilva <tilavadhruv111@gmail.com>
2023-09-19 16:50:22 +0530

    javascript separated

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
+document.addEventListener("DOMContentLoaded", init);
 
+function init() {
+    document.getElementById("plus").addEventListener("click", addTodo)
+    document.getElementById("inp").addEventListener("keypress", (event) => {
+        if (event.keyCode === 13) {
+            addTodo()
+        }
+    })
+}
 
-const addTodo = () => {
+function addTodo() {
     let userInput = document.getElementById("inp").value
     if (userInput) {
         const todo = document.createElement("li")
-        todo.innerHTML = `<span
-    class="text-white text-lg font-semibold">${userInput}</span>
-<span class="flex items-center space-x-3 !no-underline"><i
-        class="fa-solid fa-trash text-white text-xl !no-underline cursor-pointer opacity-70 transition-all duration-150 hover:opacity-100" onClick="removeTodo(this)"></i><i
-        class="fa-solid fa-check text-white text-xl !no-underline cursor-pointer opacity-70 transition-all duration-150 hover:opacity-100" onClick="checkTodo(this)"></i></span>`
         todo.className = "bg-rose-700 p-2 flex justify-between items-center mb-2"
-        document.getElementById("lists").appendChild(todo)
-        document.getElementById("inp").value = ""
+        const spanText = document.createElement("span")
+        spanText.className = "text-white text-lg font-semibold"
+        spanText.innerText = userInput
 
-        // const removeButton = document.getElementsByClassName("fa-trash")
-        // console.log(removeButton);
+        const iconContainer = document.createElement("span")
+        iconContainer.className = "flex items-center space-x-3 !no-underline"
+
+        const trash = document.createElement("i")
+        trash.className = "fa-solid fa-trash text-white text-xl !no-underline cursor-pointer opacity-70 transition-all duration-150 hover:opacity-100"
+        trash.onclick = function () {
+            this.parentElement.parentElement.remove()
+        }
+
+        const check = document.createElement("i")
+        check.className = "fa-solid fa-check text-white text-xl !no-underline cursor-pointer opacity-70 transition-all duration-150 hover:opacity-100"
+        check.onclick = function () {
+            this.parentElement.parentElement.querySelector("span").classList.toggle("line-through")
+            this.parentElement.parentElement.querySelector("span").classList.toggle("decoration-white")
+        }
+
+        iconContainer.append(trash)
+        iconContainer.append(check)
+
+        todo.append(spanText)
+        todo.append(iconContainer)
+        document.getElementById("lists").append(todo)
+        document.getElementById("inp").value = ""
         document.getElementById("inp").focus()
     }
 }
-
-document.getElementById("plus").addEventListener("click", addTodo)
-document.getElementById("inp").addEventListener("keypress", (event) => {
-    if (event.keyCode === 13) {
-        addTodo()
-    }
-})
-const removeButton = document.getElementsByClassName("fa-trash")
-
-const removeTodo = (button) => {
-    const parent = button.parentElement.parentElement
-    parent.remove()
-}
-
-
-const checkTodo = (button) => {
-    const parent = button.parentElement.parentElement.querySelector("span")
-    parent.classList.toggle("line-through")
-    parent.classList.toggle("decoration-white")
-}
-
-
